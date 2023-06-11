42a461c08038fe89bac45e4890eba8cee024b596
Mateusz Wojtyna <w_mateusz@hotmail.com>
2023-06-11 16:54:00 +0200

    fix: ProtectedRoute showed contents for a split second before authorizing

--- client/src/components/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute.tsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 
 export default function ProtectedRoute({ children }: { children: React.ReactNode }) {
 	const navigate = useNavigate();
+	const [authorized, setAuthorized] = React.useState(false);
 
 	React.useEffect(() => {
 		(async () => {
@@ -12,9 +13,10 @@ export default function ProtectedRoute({ children }: { children: React.ReactNode
 				Cookies.remove("token", { sameSite: "strict" }); // options set to avoid warning in firefox
 				return navigate("/signin");
 			}
+			setAuthorized(true);
 		})();
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
-	return <>{children}</>;
+	return <>{authorized && children}</>;
 }
