ac05e8db962451a752ed48c69795503f8e07cbe3
Mateusz Wojtyna <w_mateusz@hotmail.com>
2023-11-03 16:46:28 +0100

    feat: remove selected

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,17 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 const apiUrl = "http://localhost:3000";
 
 export interface Image {
     name: string;
     data: Blob;
+    selected: boolean;
 }
 
 function App() {
     const [images, setImages] = useState<Image[]>([]);
+    const allSelected = useMemo(() => images.every((image) => image.selected), [images]);
+    const someSelected = useMemo(() => images.some((image) => image.selected), [images]);
 
     useEffect(() => {
         setImages([]);
@@ -23,11 +26,11 @@ function App() {
         for (const name of names) {
             const res = await fetch(`${apiUrl}/image/${name}`);
             const data = await res.blob();
-            newImages.push({ name, data });
+            newImages.push({ name, data, selected: false });
         }
         setImages(newImages);
     }
-    async function deleteImage(name: string) {
+    async function removeImage(name: string) {
         await fetch(`${apiUrl}/image/${name}`, { method: "DELETE" });
         fetchImages();
     }
@@ -58,24 +61,49 @@ function App() {
     return (
         <>
             <h1 className="font-bold text-2xl">Uploaded Images</h1>
-            <div>
+            <div className="space-x-2">
                 <button onClick={fetchImages}>reload</button>
+                <button
+                    onClick={() =>
+                        setImages(images.map((image) => ({ ...image, selected: !allSelected })))
+                    }
+                >
+                    {allSelected && "de"}select all
+                </button>
+                <button
+                    onClick={() =>
+                        images.forEach((image) => {
+                            if (image.selected) {
+                                removeImage(image.name);
+                            }
+                        })
+                    }
+                    disabled={!someSelected}
+                >
+                    remove selected
+                </button>
             </div>
             <div className="flex gap-4 flex-wrap">
                 {images.map((image, i) => (
                     <div
                         key={i}
-                        className="bg-gray-700 rounded-md w-80 p-4 flex flex-col gap-4 justify-center items-center"
+                        className="bg-gray-700 rounded-md w-1/6 overflow-clip p-4 flex flex-col gap-4 justify-center items-center"
                     >
                         <h1>{image.name}</h1>
-                        <img
-                            className="object-cover"
-                            src={URL.createObjectURL(image.data)}
-                            alt={image.name}
-                        />
+                        <img src={URL.createObjectURL(image.data)} alt={image.name} />
 
-                        <button onClick={() => deleteImage(image.name)}>delete</button>
+                        <button onClick={() => removeImage(image.name)}>delete</button>
                         <button onClick={() => renameImage(image.name)}>rename</button>
+                        <input
+                            className="w-8 h-8"
+                            type="checkbox"
+                            checked={image.selected}
+                            onChange={() => {
+                                const newImages = [...images];
+                                newImages[i].selected = !newImages[i].selected;
+                                setImages(newImages);
+                            }}
+                        />
                     </div>
                 ))}
             </div>
